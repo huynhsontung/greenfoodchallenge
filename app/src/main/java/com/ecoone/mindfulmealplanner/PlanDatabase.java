b8e09bf6cd073cf079fbeeb90156eeb93351bd89
Tom <tung75605@gmail.com>
2018-10-14 01:51:03 -0700

    WIP: Database Implementation Requires different thread to access database.

--- app/src/main/java/com/ecoone/mindfulmealplanner/PlanDatabase.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/PlanDatabase.java
@@ -1,6 +1,8 @@
 package com.ecoone.mindfulmealplanner;
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import 	android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 // Database object implemented by Room.
 // Please read https://developer.android.com/training/data-storage/room/
@@ -9,6 +11,19 @@ import 	android.arch.persistence.room.RoomDatabase;
 @Database(entities = Plan.class, version = 1)
 public abstract class PlanDatabase extends RoomDatabase {
     public abstract PlanDao PlanDao();
+
+    private static volatile PlanDatabase INSTANCE;
+
+    static PlanDatabase getDatabase(final Context context){
+        if (INSTANCE ==  null){
+            synchronized (PlanDatabase.class){
+                if (INSTANCE == null){
+                    INSTANCE = Room.databaseBuilder(context,PlanDatabase.class,"PlanDatabase").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
 
 
