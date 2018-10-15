e48081af97d8099b6478cadbd050261b1d6fe4ea
Tom <tung75605@gmail.com>
2018-10-14 17:21:25 -0700

    Added more queries and made queries runnable on main thread.

--- app/src/main/java/com/ecoone/mindfulmealplanner/PlanDao.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/PlanDao.java
@@ -1,10 +1,15 @@
 package com.ecoone.mindfulmealplanner;
 
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
 
 // Database object implemented by Room.
 // Please read https://developer.android.com/training/data-storage/room/
@@ -13,13 +18,26 @@ import android.arch.persistence.room.Query;
 @Dao
 public interface PlanDao {
     @Query("SELECT * FROM `Plan` ORDER BY `key` DESC LIMIT 1")
-    public Plan loadCurrentPlan();
+    public Plan getCurrentPlan();
+
+    @Query("SELECT * FROM `Plan`")
+    public List<Plan> getAllPlans();
+
+    @Query("SELECT count(*) FROM `Plan`")
+    public int getPlansCount();
 
     @Insert
     public void addPlan(Plan plan);
 
+    @Update
+    public int updatePlan(Plan plan);
+
     @Delete
-    public void deletePlan(Plan plan);
+    public int deletePlan(Plan plan);
+
+    @Query("DELETE FROM `Plan`")
+    public void deleteAllPlans();
+
 
 
 }
