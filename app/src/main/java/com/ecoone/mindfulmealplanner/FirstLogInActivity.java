f4a746c06d6023efd43fc07a46930fc594cede44
arlenx <arlenxuzj@gmail.com>
2018-10-17 22:59:44 -0700

    comment some code in DasgboardActivity.

--- app/src/main/java/com/ecoone/mindfulmealplanner/FirstLogInActivity.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/FirstLogInActivity.java
@@ -2,6 +2,9 @@ package com.ecoone.mindfulmealplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
 
 public class FirstLogInActivity extends AppCompatActivity {
 
