70f9b91781ce0beb46ad1fe7b3e76bb63f2e3204
arlenx <arlenxuzj@gmail.com>
2018-10-17 23:13:55 -0700

    add plot library "MPAndroidChart" and test

--- app/src/main/java/com/ecoone/mindfulmealplanner/FirstLogInActivity.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/FirstLogInActivity.java
@@ -6,11 +6,34 @@ import android.view.View;
 import android.widget.TextView;
 import android.content.Intent;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class FirstLogInActivity extends AppCompatActivity {
+    private LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_log_in);
+
+        chart = findViewById(R.id.chart);
+
+        List<Entry> entries = new ArrayList<>();
+
+        entries.add(new Entry(1,1));
+        entries.add(new Entry(2,2));
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label");
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate();
     }
 }
