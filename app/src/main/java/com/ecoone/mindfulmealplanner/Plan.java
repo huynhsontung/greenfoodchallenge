9f56826942533c98226c115de1fd9a391f1f65f3
Tom <tung75605@gmail.com>
2018-10-20 03:03:43 -0700

    Bug fix

--- app/src/main/java/com/ecoone/mindfulmealplanner/Plan.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/Plan.java
@@ -3,9 +3,8 @@ package com.ecoone.mindfulmealplanner;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.ForeignKey;
-import android.arch.persistence.room.PrimaryKey;
-
-import io.reactivex.annotations.NonNull;
+import android.arch.persistence.room.Index;
+import android.support.annotation.NonNull;
 
 import static android.arch.persistence.room.ForeignKey.CASCADE;
 
@@ -16,15 +15,14 @@ import static android.arch.persistence.room.ForeignKey.CASCADE;
 @Entity(foreignKeys = @ForeignKey(entity = User.class,
         parentColumns = "username",
         childColumns = "username",
-        onDelete = CASCADE))
+        onDelete = CASCADE),
+        primaryKeys = {"planName", "username"})
 public class Plan {
 
 
-    @PrimaryKey
     @NonNull
     public String planName;
 
-    @PrimaryKey
     @NonNull
     public String username;
 
--- app/src/main/java/com/ecoone/mindfulmealplanner/User.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/User.java
@@ -2,11 +2,13 @@ package com.ecoone.mindfulmealplanner;
 
 import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
-import io.reactivex.annotations.NonNull;
 
 @Entity
 public class User {
+
+
     @PrimaryKey
     @NonNull
     public String username; // Name has to be unique
