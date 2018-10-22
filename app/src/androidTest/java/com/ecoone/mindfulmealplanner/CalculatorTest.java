628b19c1428f76053a9d1f1fc57f95f06bd3ef46
Peter Tran <ttt13@sfu.ca>
2018-10-21 19:46:57 -0700

    Adding tests

---
+++ app/src/androidTest/java/com/ecoone/mindfulmealplanner/CalculatorTest.java
@@ -0,0 +1,32 @@
+package com.ecoone.mindfulmealplanner;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testCalculateCO2e() {
+    }
+
+    @Test
+    public void testComparePlan() {
+    }
+
+    @Test
+    public void testSumServings() {
+    }
+
+    @Test
+    public void testGetScalingFactor() {
+    }
+
+    @Test
+    public void testCalculateVancouver() {
+    }
+
+    @Test
+    public void testUsePlanVancouver() {
+    }
+}
