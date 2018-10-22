e483d70ca61e307aef83f746d4de2564eee84e06
arlenx <arlenxuzj@gmail.com>
2018-10-21 23:21:15 -0700

    Merge remote-tracking branch 'remotes/origin/10-combine-branch2-and-branch5' into 5-implement-dashboard-ui
    # Conflicts:
    #	app/build.gradle
    #	app/schemas/com.ecoone.mindfulmealplanner.AppDatabase/1.json
    #	app/src/main/java/com/ecoone/mindfulmealplanner/Dashboard.java
    #	app/src/main/java/com/ecoone/mindfulmealplanner/Plan.java
    #	app/src/main/java/com/ecoone/mindfulmealplanner/PlanDao.java
    #	app/src/main/java/com/ecoone/mindfulmealplanner/User.java
    #	app/src/main/java/com/ecoone/mindfulmealplanner/UserDao.java
    #	app/src/main/java/com/ecoone/mindfulmealplanner/db/AppDatabase.java
    #	app/src/main/res/layout/content_dashboard.xml

--- app/src/main/java/com/ecoone/mindfulmealplanner/fragments/DashboardFragment.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/fragments/DashboardFragment.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ecoone.mindfulmealplanner.MainActivity;
 import com.ecoone.mindfulmealplanner.R;
@@ -21,6 +22,7 @@ import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +40,10 @@ public class DashboardFragment extends Fragment {
     private AppDatabase mDb;
     private dbInterface mDbInterface;
 
-    private PieChart mPieChart;
+
+    private Button improveButton;
+    private PieChart chart1;
+    private PieChart chart2;
 
     private static final String TAG = "testActivity";
 
@@ -59,19 +64,109 @@ public class DashboardFragment extends Fragment {
         mDb = AppDatabase.getDatabase(getContext());
         mDbInterface = new dbInterface(mDb);
 
-
-        mPieChart = view.findViewById(R.id.fragment_dashboard_pie_chart);
+        foodName = findStringArrayRes("food_name");
+        foodLen = foodName.length;
 
         mUsername = getArguments().getString(MainActivity.EXTRA_USERNAME);
         mGender = mDbInterface.getGenderbyUsername(mUsername);
         mCurrentPlan = mDbInterface.getCurrentPlanNamebyUsername(mUsername);
+        foodAmount = mDbInterface.getCurrentPlanArray(mUsername, mCurrentPlan);
+
+        improveButton = view.findViewById(R.id.imp);
+        chart1= view.findViewById(R.id.PieChart1);
+        chart2= view.findViewById(R.id.PieChart2);
 
         Log.i(TAG, "Name in dashboard fragment: " + mUsername);
 
-        foodName = findStringArrayRes("food_name");
-        foodLen = foodName.length;
-        foodAmount = mDbInterface.getCurrentPlanArray(mUsername, mCurrentPlan);
-        setPieChartView(foodAmount);
+        int beefInGram = foodAmount[0];
+        int porkInGram = foodAmount[1];
+        int chickenInGram = foodAmount[2];
+        int fishInGram = foodAmount[3];
+        int eggsInGram = foodAmount[4];
+        int beansInGram = foodAmount[5];
+        int vegetablesInGram = foodAmount[6];
+
+        float beefco2e = beefInGram * 27/1000;
+        float porkco2e = porkInGram* 12/1000;
+        float chickenco2e = chickenInGram*7/1000;
+        float fishco2e = fishInGram*6/1000;
+        float eggsco2e = eggsInGram*5/1000;
+        float beansco2e = beansInGram*2/1000;
+        float vegetablesco2e = vegetablesInGram*2/1000;
+
+        float sumco2e = beefco2e+porkco2e+chickenco2e+fishco2e+eggsco2e+beansco2e+vegetablesco2e;
+
+        float beefco2per = beefco2e/sumco2e;
+        float porkco2per = porkco2e/sumco2e;
+        float chickenco2per = chickenco2e/sumco2e;
+        float fishco2per = fishco2e/sumco2e;
+        float eggsco2per = eggsco2e/sumco2e;
+        float beansco2per = beansco2e/sumco2e;
+        float vegetablesco2per = vegetablesco2e/sumco2e;
+
+
+        float sumInGram = beefInGram+porkInGram+chickenInGram+fishInGram+eggsInGram+beansInGram+vegetablesInGram;
+
+        float beefPercentage = beefInGram/sumInGram;
+        float porkPercentage = porkInGram/sumInGram;
+        float chickenPercentage = chickenInGram/sumInGram;
+        float fishPercentage = fishInGram/sumInGram;
+        float eggsPercentage = eggsInGram/sumInGram;
+        float beansPercentage = beansInGram/sumInGram;
+        float vegetablesPercentage = vegetablesInGram/sumInGram;
+
+
+
+        float percentage[] ={ beefPercentage, porkPercentage, chickenPercentage, fishPercentage , eggsPercentage, beansPercentage, vegetablesPercentage};
+        float co2Percentage[] = {beefco2per,porkco2per, chickenco2per,fishco2per,eggsco2per,beansco2per,vegetablesco2per};
+
+        setupImproveButton();
+        setupPieChart1(percentage, foodName);
+        setupPieChart2(co2Percentage,foodName);
+
+    }
+
+    private void setupImproveButton(){
+        //Wire up the button to improve the plan
+        //...get the button
+        improveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Clicked 'improve'.", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+
+    private void setupPieChart1(float[] percentage, String[] foodNames){
+        //setup pie chart
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i=0; i<percentage.length;i++){
+            pieEntries.add(new PieEntry(percentage[i], foodNames[i]));
+        }
+
+        PieDataSet dataset = new PieDataSet(pieEntries,"Current grams");
+        dataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataset);
+        chart1.setData(data);
+        chart1.animateY(1000);
+        chart1.invalidate();
+    }
+
+
+    private void setupPieChart2(float[] co2Percentage, String[] foodNames){
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i=0; i<co2Percentage.length;i++){
+            pieEntries.add(new PieEntry(co2Percentage[i], foodNames[i]));
+        }
+
+        PieDataSet dataSet = new PieDataSet(pieEntries,"Current co2e");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData data = new PieData(dataSet);
+
+        chart2.setData(data);
+        chart2.animateY(1000);
+        chart2.invalidate();
     }
 
 
@@ -81,17 +176,17 @@ public class DashboardFragment extends Fragment {
         return getResources().getStringArray(resId);
     }
 
-    private void setPieChartView(int[] data) {
-        List<PieEntry> entries = new ArrayList<>();
-        for (int i = 0; i < foodLen; i++) {
-            entries.add(new PieEntry(data[i], foodName[i]));
-        }
-        PieDataSet pieDataSet = new PieDataSet(entries, "Test");
-        PieData piedata = new PieData(pieDataSet);
-        mPieChart.setData(piedata);
-        mPieChart.invalidate();
-
-    }
+//    private void setPieChartView(int[] data) {
+//        List<PieEntry> entries = new ArrayList<>();
+//        for (int i = 0; i < foodLen; i++) {
+//            entries.add(new PieEntry(data[i], foodName[i]));
+//        }
+//        PieDataSet pieDataSet = new PieDataSet(entries, "Test");
+//        PieData piedata = new PieData(pieDataSet);
+//        mPieChart.setData(piedata);
+//        mPieChart.invalidate();
+//
+//    }
 
     private static int randInt(int min, int max) {
         Random rand = new Random();
