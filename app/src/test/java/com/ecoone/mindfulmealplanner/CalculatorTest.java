c7b18263f9433fdf8d1ce3bd87f6fdb2b7186b95
Peter Tran <ttt13@sfu.ca>
2018-10-25 17:42:02 -0700

    Adding more test cases, moved to java testing folder

--- app/src/test/java/com/ecoone/mindfulmealplanner/CalculatorTest.java
+++ app/src/test/java/com/ecoone/mindfulmealplanner/CalculatorTest.java
@@ -3,6 +3,10 @@ package com.ecoone.mindfulmealplanner;
 import com.ecoone.mindfulmealplanner.db.Plan;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CalculatorTest {
@@ -20,7 +24,7 @@ public class CalculatorTest {
     };
 
     @Test
-    public void testCalculateCO2e() {
+    public void testCalculateCO2ePerYear() {
         Calculator myCalculator = new Calculator();
         float getCO2e = myCalculator.calculateCO2ePerYear(testPlan);
 
@@ -41,6 +45,24 @@ public class CalculatorTest {
     }
 
     @Test
+    public void testCalculateCO2ePerDay() {
+        Calculator myCalculator = new Calculator();
+        float getCO2e = myCalculator.calculateCO2ePerDay(testPlan);
+
+        float getCO2eManually = 0;
+        getCO2eManually += testPlan.beef * 27;
+        getCO2eManually += testPlan.pork * 12.1;
+        getCO2eManually += testPlan.chicken * 6.9;
+        getCO2eManually += testPlan.fish * 6.1;
+        getCO2eManually += testPlan.eggs * 4.8;
+        getCO2eManually += testPlan.beans * 2;
+        getCO2eManually += testPlan.vegetables * 2;
+
+        assertEquals(getCO2e, getCO2eManually, 1);
+
+    }
+
+    @Test
     public void testComparePlan() {
         Calculator myCalculator = new Calculator();
         float testCO2e = 2;
@@ -49,6 +71,22 @@ public class CalculatorTest {
         float getComparison = myCalculator.comparePlan(2, testPlan);
         //assert(getComparison == testCO2e);
         assertEquals(testCO2e, getComparison, 1); // Delta denotes max loss in precision allowed.
+    }
+
+    @Test
+    public void testSumCO2ePerDayPlanList(){
+        Calculator myCalculator = new Calculator();
+
+        List<Plan> testList = new ArrayList<>();
+        testList.add(testPlan);
+        testList.add(testPlan);
+
+        float getSumManually = myCalculator.calculateCO2ePerDay(testPlan);
+        getSumManually *= 2;
+
+        float getSum = myCalculator.sumCO2ePerDayPlanList(testList);
+
+        assertEquals(getSumManually, getSum, 1);
     }
 
     @Test
@@ -96,5 +134,48 @@ public class CalculatorTest {
         float getNewCO2eTest = myCalculator.usePlanVancouver(testPlan);
 
         assertEquals(manualCalculationVancouver, getNewCO2eTest, 1);
+    }
+
+    @Test
+    public void testCalculateCO2eEachFood(){
+        float[] testCO2eAmount = new float[7];
+        Calculator myCalculator = new Calculator();
+
+        testCO2eAmount = myCalculator.calculateCO2eEachFood(testPlan);
+
+        assertEquals(50*27, testCO2eAmount[0],0);
+        assertEquals(75*12.1, testCO2eAmount[1],0.1);
+        assertEquals(50*6.9,testCO2eAmount[2], 0.1);
+        assertEquals(50*6.1, testCO2eAmount[3],0.1);
+        assertEquals(50*4.8, testCO2eAmount[4], 0.1);
+        assertEquals(25*2, testCO2eAmount[5], 0.1);
+        assertEquals(100*2, testCO2eAmount[6], 0.1);
+    }
+
+    @Test
+    public void testCalculateSavingsInKm(){
+        Calculator myCalculator = new Calculator();
+        float testCO2eManually = 2;
+
+        float getSavings = myCalculator.calculateSavingsInKm(2);
+
+        testCO2eManually *= 1000000;
+        testCO2eManually /= 178;
+
+        assertEquals(testCO2eManually, getSavings, 1);
+    }
+
+    @Test
+    public void testCalculateTreesPlanted(){
+        Calculator myCalculator = new Calculator();
+        float testCO2eManually = 2;
+        float savedCO2e = (float) 0.871;
+
+        testCO2eManually = testCO2eManually / savedCO2e;
+        testCO2eManually = Math.round(testCO2eManually);
+
+        float getTreesPlanted = myCalculator.calculateTreesPlanted(2);
+
+        assertEquals(testCO2eManually, getTreesPlanted, 0);
     }
 }
