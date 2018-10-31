5e359dadbb734054209b00a985b977c15f0f2cb0
Peter Tran <ttt13@sfu.ca>
2018-10-30 19:47:45 -0700

    Just starting to get the fragment to appear

---
+++ app/src/main/java/com/ecoone/mindfulmealplanner/fragments/PledgeFragment.java
@@ -0,0 +1,48 @@
+package com.ecoone.mindfulmealplanner.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ecoone.mindfulmealplanner.DbInterface;
+import com.ecoone.mindfulmealplanner.MainActivity;
+import com.ecoone.mindfulmealplanner.R;
+import com.ecoone.mindfulmealplanner.db.AppDatabase;
+
+
+public class PledgeFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    public PledgeFragment() {
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_pledge, null );
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
