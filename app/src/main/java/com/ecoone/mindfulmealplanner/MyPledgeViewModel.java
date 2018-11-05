7c8238674947d1abb0799025e79a0eed02300ca8
Peter Tran <ttt13@sfu.ca>
2018-11-05 10:17:53 -0800

    Create view model for my pledge

---
+++ app/src/main/java/com/ecoone/mindfulmealplanner/MyPledgeViewModel.java
@@ -0,0 +1,39 @@
+package com.ecoone.mindfulmealplanner;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.ecoone.mindfulmealplanner.db.Plan;
+
+
+public class MyPledgeViewModel extends ViewModel {
+
+    private static float myPledge;
+    private static String myUserName;
+    private static String planName;
+    private static Plan myPlan;
+
+    // Post: Updates database if user changes their pledge.
+    public static void setCurrentPledge(){
+
+    }
+
+    // Post: Gets user's pledge from the database.
+    public static void getCurrentPledge(){
+
+    }
+
+    // Post: Gets current username (For the database)
+    public static void getUserName(){
+
+    }
+
+    // Post: Gets the user's current plan name
+    public static void getPlanName(){
+
+    }
+
+    // Post: Gets the user's current plan.
+    public static void getPlan(){
+
+    }
+}
