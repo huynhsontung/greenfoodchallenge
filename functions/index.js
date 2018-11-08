b6daf341bfdb2577d346ea0860dc73a58391f91b
ArlenX <arlenxuzj@gmail.com>
2018-11-07 19:59:58 -0800

    restructure the firebase

--- functions/index.js
+++ functions/index.js
@@ -3,40 +3,17 @@ const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 
 admin.initializeApp();
-//
-// exports.getUserGender = functions.https.onCall((data, context) => {
-//   const userUid = data.userUid;
-//
-//   return admin.database().ref('/users/' + userUid)
-//       .once('value')
-//       .then((snapshot) => {
-//           console.log("Check snapshot" + snapshot.child("gender").val());
-//           return snapshot.child("gender").val();
-//       });
-// });
-//
-// exports.getSumPledge = functions.https.onCall((data, context ) => {
-//    var sum = 0;
-//    var query = admin.database().ref("/users");
-//    return query.once("value")
-//        .then((snapshot) => {
-//            snapshot.forEach((childSnapshot) =>{
-//                sum = sum + childSnapshot.child("pledge").val();
-//            });
-//            return sum;
-//        });
-// });
-//
-exports.addTotalNumberOfPeoplePledged = functions.database.ref("/{userUid}/pledgeInfo")
+
+exports.addTotalNumberOfPeoplePledged = functions.database.ref("/uids/{userUid}/pledgeInfo")
     .onWrite((change, context) => {
         var totalNumber = 0;
         var totalAmountSum = 0;
-        var query1 = admin.database().ref("/users");
+        var query1 = admin.database().ref("/uids");
         var query2 = admin.database().ref("/pledgeResult");
         return query1.once("value")
             .then((snapshot) => {
                 snapshot.forEach((childSnapshot) => {
-                    var amount = childSnapshot.child("pledge").child("amount").val();
+                    var amount = childSnapshot.child("pledgeInfo").child("amount").val();
                     // console.log("test", amount);
                     if (amount !== 0) {
                         totalNumber ++;
