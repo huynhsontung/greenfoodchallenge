30822fc8afe624d67e0b7f6f9d8bf663097f9dfb
Peter Tran <ttt13@sfu.ca>
2018-11-10 18:29:54 -0800

    Playing around. Can now use camera or upload photo

--- app/src/main/java/com/ecoone/mindfulmealplanner/AddGreenMealActivity.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/AddGreenMealActivity.java
@@ -1,6 +1,7 @@
 package com.ecoone.mindfulmealplanner;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
@@ -13,7 +14,8 @@ import android.widget.ImageView;
 public class AddGreenMealActivity extends AppCompatActivity {
 
     private static final int RESULT_LOAD_IMAGE = 1;
-    ImageView imageToUpload;
+    private static final int RESULT_CAMERA_IMAGE = 2;
+    ImageView imageToUpload, mealImage, imageFromCamera;
     EditText getMealName;
 
     @Override
@@ -22,12 +24,22 @@ public class AddGreenMealActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_green_meal);
 
         imageToUpload = (ImageView)findViewById(R.id.uploadImage);
+        mealImage = (ImageView)findViewById(R.id.meal_image_view);
+        imageFromCamera = (ImageView)findViewById(R.id.camera_image_view);
 
         imageToUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        imageFromCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, RESULT_CAMERA_IMAGE);
             }
         });
     }
@@ -38,7 +50,12 @@ public class AddGreenMealActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
             Uri selectedImage = data.getData(); // uniform resource indicator
-            imageToUpload.setImageURI(selectedImage);
+            mealImage.setImageURI(selectedImage);
+        }
+
+        if(requestCode == RESULT_CAMERA_IMAGE && resultCode == RESULT_OK && data != null){
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            mealImage.setImageBitmap(bitmap);
         }
     }
 }
