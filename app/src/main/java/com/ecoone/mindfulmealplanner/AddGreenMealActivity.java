dfb83ce9e3f1eb1dce274974b25681f67664fb98
Peter Tran <ttt13@sfu.ca>
2018-11-12 21:51:19 -0800

    Move photo abilities to viewpager

--- app/src/main/java/com/ecoone/mindfulmealplanner/AddGreenMealActivity.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/AddGreenMealActivity.java
@@ -16,7 +16,7 @@ public class AddGreenMealActivity extends AppCompatActivity {
 
     private static final int RESULT_LOAD_IMAGE = 1;
     private static final int RESULT_CAMERA_IMAGE = 2;
-    ImageView imageToUpload, mealImage, imageFromCamera;
+
     EditText getMealName;
     private ViewPager viewPager;
     private AddPhotoFragmentAdapter adapter;
@@ -26,42 +26,14 @@ public class AddGreenMealActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_green_meal);
 
-        imageToUpload = (ImageView)findViewById(R.id.uploadImage);
-        mealImage = (ImageView)findViewById(R.id.meal_image_view);
-        imageFromCamera = (ImageView)findViewById(R.id.camera_image_view);
+
         viewPager = findViewById(R.id.image_pager);
+        viewPager.setOffscreenPageLimit(6);
         adapter = new AddPhotoFragmentAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
 
-        imageToUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
-            }
-        });
-
-        imageFromCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, RESULT_CAMERA_IMAGE);
-            }
-        });
     }
 
     // When user selects image from gallery
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
-            Uri selectedImage = data.getData(); // uniform resource indicator
-            mealImage.setImageURI(selectedImage);
-        }
 
-        if(requestCode == RESULT_CAMERA_IMAGE && resultCode == RESULT_OK && data != null){
-            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-            mealImage.setImageBitmap(bitmap);
-        }
-    }
 }
--- app/src/main/java/com/ecoone/mindfulmealplanner/AddPhotoFragment.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/AddPhotoFragment.java
@@ -1,12 +1,20 @@
 package com.ecoone.mindfulmealplanner;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -14,6 +22,9 @@ import android.widget.TextView;
  */
 public class AddPhotoFragment extends Fragment {
 
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private static final int RESULT_CAMERA_IMAGE = 2;
+    ImageView imageToUpload, mealImage, imageFromCamera;
     private TextView textView;
     public AddPhotoFragment() {
         // Required empty public constructor
@@ -25,11 +36,43 @@ public class AddPhotoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_photo, container, false);
-
+        imageToUpload = (ImageView)view.findViewById(R.id.uploadImage);
+        mealImage = (ImageView)view.findViewById(R.id.meal_image_view);
+        imageFromCamera = (ImageView)view.findViewById(R.id.camera_image_view);
         textView = view.findViewById(R.id.test_text_view);
         String message = getArguments().getString("message");
         textView.setText(message);
+
+        imageToUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        imageFromCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, RESULT_CAMERA_IMAGE);
+            }
+        });
+
         return view;
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE  && data != null){
+            Uri selectedImage = data.getData(); // uniform resource indicator
+            mealImage.setImageURI(selectedImage);
+        }
+
+        if(requestCode == RESULT_CAMERA_IMAGE && resultCode == RESULT_OK && data != null){
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            mealImage.setImageBitmap(bitmap);
+        }
+    }
 }
