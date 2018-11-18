a90bb00a192e006ee32dd6f5be998b0da6e19152
Peter Tran <ttt13@sfu.ca>
2018-11-18 00:52:25 -0800

    Adding auto complete

--- app/src/main/java/com/ecoone/mindfulmealplanner/AddGreenMealActivity.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/AddGreenMealActivity.java
@@ -5,12 +5,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.SearchView;
 
 public class AddGreenMealActivity extends AppCompatActivity {
 
-    private static final String[] restaurantSuggestions = {
-            "A&W", "Tim Hortons", "Pizza Hut", "Liquor Store", "Subway", "SFU Dining Hall", "Togo Sushi"
+    private AutoCompleteTextView autoCompleteTextView;
+
+    private static final String[] RESTAURANT_SUGGESTIONS = {
+            "A&W", "Tim Hortons", "Pizza Hut", "Liquor Store",
+            "Subway", "SFU Dining Hall", "Togo Sushi", "Nester's Market", "Starbucks"
     };
 
     public static Intent newIntent(Context packageContext) {
@@ -23,6 +29,11 @@ public class AddGreenMealActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_green_meal);
+
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(AddGreenMealActivity.this,
+                android.R.layout.simple_list_item_1, RESTAURANT_SUGGESTIONS);
+        autoCompleteTextView.setAdapter(adapter);
 
         goToPhoto = (Button)findViewById(R.id.add_green_meal_startphoto_button);
         goToPhoto.setOnClickListener(new View.OnClickListener() {
