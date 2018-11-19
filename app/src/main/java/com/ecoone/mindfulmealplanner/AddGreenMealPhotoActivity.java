9e564667bc1a942a0cee7f95ddd9f9082fbaf8c6
Peter Tran <ttt13@sfu.ca>
2018-11-18 20:46:13 -0800

    New add photo image, adding dynamic auto complete

--- app/src/main/java/com/ecoone/mindfulmealplanner/AddGreenMealPhotoActivity.java
+++ app/src/main/java/com/ecoone/mindfulmealplanner/AddGreenMealPhotoActivity.java
@@ -2,15 +2,30 @@ package com.ecoone.mindfulmealplanner;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.MultiAutoCompleteTextView;
 
 public class AddGreenMealPhotoActivity extends AppCompatActivity {
 
     private static final int RESULT_LOAD_IMAGE = 1;
     private static final int RESULT_CAMERA_IMAGE = 2;
+
+    private static final String[] GREEN_MEALS = {
+            "Pizza", "Beyond Meat Burger", "California Roll", "Vodka"
+    };
+
+    private AutoCompleteTextView autoCompleteTextView;
+    private ImageView plusFoodItem;
 
     EditText getMealName;
     private ViewPager viewPager;
@@ -31,6 +46,29 @@ public class AddGreenMealPhotoActivity extends AppCompatActivity {
         viewPager.setOffscreenPageLimit(6);
         adapter = new AddPhotoFragmentAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
+
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView2);
+        plusFoodItem = findViewById(R.id.add_green_meal_photo_addtextview);
+
+        plusFoodItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addLine();
+            }
+        });
+
+    }
+
+    private void addLine() {
+        ConstraintLayout cl = (ConstraintLayout)findViewById(R.id.add_green_meal_photo_constraintlayout);
+        ConstraintSet set = new ConstraintSet();
+
+        AutoCompleteTextView addNewAutoComplete = new AutoCompleteTextView(this);
+
+        ConstraintLayout.LayoutParams p = new ConstraintLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        addNewAutoComplete.setLayoutParams(p);
+        cl.addView(addNewAutoComplete);
 
     }
 
